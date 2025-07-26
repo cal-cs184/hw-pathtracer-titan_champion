816a1141ce4758d11b2266186374d256c32e1457
Phone Min <phonemyatmin@berkeley.edu>
2025-07-25 23:13:34 -0700

    part 2 brush up

--- src/scene/bbox.cpp
+++ src/scene/bbox.cpp
@@ -13,9 +13,34 @@ bool BBox::intersect(const Ray& r, double& t0, double& t1) const {
   // Implement ray - bounding box intersection test
   // If the ray intersected the bouding box within the range given by
   // t0, t1, update t0 and t1 with the new intersection times.
+    Vector3D x_hat(1, 0, 0);
+    double tx0 = dot((min - r.o), x_hat) / dot(r.d, x_hat);
+    double tx1 = dot((max - r.o), x_hat) / dot(r.d, x_hat);
+    if (tx1 < tx0) std::swap(tx0, tx1);
 
+    Vector3D y_hat(0, 1, 0);
+    double ty0 = dot((min - r.o), y_hat) / dot(r.d, y_hat);
+    double ty1 = dot((max - r.o), y_hat) / dot(r.d, y_hat);
+    if (ty1 < ty0) std::swap(ty0, ty1);
 
-  return true;
+    Vector3D z_hat(0, 0, 1);
+    double tz0 = dot((min - r.o), z_hat) / dot(r.d, z_hat);
+    double tz1 = dot((max - r.o), z_hat) / dot(r.d, z_hat);
+    if (tz1 < tz0) std::swap(tz0, tz1);
+
+    double t_min = std::max(tx0, ty0);
+    double t_max = std::min(tx1, ty1);
+    if (t_min > t_max) return false;
+
+    if (tz0 > t_max || tz1 < t_min) return false;
+    t_min = std::max(tz0, t_min);
+    t_max = std::min(tz1, t_max);
+
+    if (t_min > t_max) return false;
+
+    // overlap between ray and box intersection interval
+    if (!((t1 - t_min) >= 0 && (t_max - t0) >= 0)) return false;
+    return true;
 
 }
 
--- src/scene/bvh.cpp
+++ src/scene/bvh.cpp
@@ -57,22 +57,84 @@ BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
   // size configuration. The starter code build a BVH aggregate with a
   // single leaf node (which is also the root) that encloses all the
   // primitives.
-
-
-  BBox bbox;
-
-  for (auto p = start; p != end; p++) {
-    BBox bb = (*p)->get_bbox();
-    bbox.expand(bb);
-  }
-
-  BVHNode *node = new BVHNode(bbox);
-  node->start = start;
-  node->end = end;
-
-  return node;
-
-
+  // Create bounding box
+    BBox bbox;
+    for (auto p = start; p != end; p++) {
+        BBox bb = (*p)->get_bbox();
+        bbox.expand(bb);
+    }
+    BVHNode* node = new BVHNode(bbox);
+    int num_primitives = (end - start);
+    if (num_primitives <= max_leaf_size) {
+        node->l = NULL;
+        node->r = NULL;
+        node->start = start;
+        node->end = end;
+    }
+    else {
+        Vector3D mean(0, 0, 0);
+        for (auto p = start; p != end; p++) {
+            mean += (*p)->get_bbox().centroid();
+        }
+        mean /= (float)num_primitives;
+        int min_split_axis = 0;            
+        float min_BBox_heuristic = INFINITY;   // heuristic
+        for (int axis = 0; axis < 3; axis++) {
+            std::vector<Primitive*> left_primitives;
+            std::vector<Primitive*> right_primitives;
+            for (auto p = start; p != end; p++) {
+                if ((*p)->get_bbox().centroid()[axis] <= mean[axis]) {
+                    left_primitives.push_back(*p);
+                }
+                else {
+                    right_primitives.push_back(*p);
+                }
+            }
+            BBox left_bbox;
+            BBox right_bbox;
+            for (int i = 0; i < left_primitives.size(); i++) {
+                BBox bb = left_primitives[i]->get_bbox();
+                left_bbox.expand(bb);
+            }
+            for (int j = 0; j < right_primitives.size(); j++) {
+                BBox bb = right_primitives[j]->get_bbox();
+                right_bbox.expand(bb);
+            }
+            Vector3D exL = left_bbox.extent;
+            Vector3D exR = right_bbox.extent;
+            float heuristic = left_primitives.size() * (exL.x * exL.y + exL.y * exL.z + exL.z * exL.x)
+                + right_primitives.size() * (exR.x * exR.y + exR.y * exR.z + exR.z * exR.x);
+            if (heuristic < min_BBox_heuristic) {
+                min_BBox_heuristic = heuristic;
+                min_split_axis = axis;
+            }
+        }
+        std::vector<Primitive*> left_primitives;
+        std::vector<Primitive*> right_primitives;
+        for (auto p = start; p != end; p++) {
+            if ((*p)->get_bbox().centroid()[min_split_axis] <= mean[min_split_axis]) {
+                left_primitives.push_back(*p);
+            }
+            else {
+                right_primitives.push_back(*p);
+            }
+        }
+        auto center_left = start;
+        auto center_right = start;
+        for (int i = 0; i < num_primitives; i++) {
+            if (i < left_primitives.size()) {
+                *center_left = left_primitives[i];
+                center_left++;
+            }
+            else {
+                *center_right = right_primitives[i - left_primitives.size()];
+            }
+            center_right++;
+        }
+        node->l = construct_bvh(start, center_left, max_leaf_size);
+        node->r = construct_bvh(center_left, end, max_leaf_size);
+    }
+    return node;
 }
 
 bool BVHAccel::has_intersection(const Ray &ray, BVHNode *node) const {
@@ -80,34 +142,46 @@ bool BVHAccel::has_intersection(const Ray &ray, BVHNode *node) const {
   // Fill in the intersect function.
   // Take note that this function has a short-circuit that the
   // Intersection version cannot, since it returns as soon as it finds
-  // a hit, it doesn't actually have to find the closest hit.
-
-
-
-  for (auto p : primitives) {
-    total_isects++;
-    if (p->has_intersection(ray))
-      return true;
-  }
-  return false;
-
-
+    if (node->bb.intersect(ray, ray.min_t, ray.max_t)) {
+        if (node->isLeaf()) {
+            for (auto p = node->start; p != node->end; p++) {
+                if ((*p)->has_intersection(ray)) {
+                    return true;
+                }
+            }
+        }
+        else {
+            return has_intersection(ray, node->l) || has_intersection(ray, node->r);
+        }
+    }
+    else {
+        return false;
+    }
 }
 
 bool BVHAccel::intersect(const Ray &ray, Intersection *i, BVHNode *node) const {
   // TODO (Part 2.3):
   // Fill in the intersect function.
-
-
-
-  bool hit = false;
-  for (auto p : primitives) {
-    total_isects++;
-    hit = p->intersect(ray, i) || hit;
-  }
-  return hit;
-
-
+    total_isects++; 
+    if (node->bb.intersect(ray, ray.min_t, ray.max_t)) {
+        if (node->isLeaf()) {
+            bool hit = false;
+            for (auto p = node->start; p != node->end; p++) {
+                total_isects++;
+                bool p_hit = (*p)->intersect(ray, i);
+                hit = hit || p_hit;
+            }
+            return hit;
+        }
+        else {
+            bool hit_left = intersect(ray, i, node->l);
+            bool hit_right = intersect(ray, i, node->r);
+            return hit_left || hit_right;
+        }
+    }
+    else {
+        return false;
+    }
 }
 
 } // namespace SceneObjects
